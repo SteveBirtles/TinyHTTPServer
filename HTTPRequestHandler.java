0a25565b47646210b0e12a7d45c31b95101da4aa
Staff   BIRTLES Steve <00216@SFCF.local>
2016-11-09 11:40:20 +0000

    Initial commit

---
+++ HTTPRequestHandler.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+import java.net.Inet4Address;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class HTTPRequestHandler extends AbstractHandler {
+
+    private StringBuilder history;
+
+    public HTTPRequestHandler()
+    {
+        history = new StringBuilder();
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException {                 
+        response.setContentType("text/html; charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        if (request.getRequestURI().equals("/favicon.ico")) return; // SKIP FAVICON REQUESTS;
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date dateobj = new Date();
+        System.out.println();
+
+        String requestText = "[" + request.getRemoteAddr()  + "  |  " + df.format(dateobj) + " | ";
+        requestText += request.getMethod() + "] " + request.getRequestURI() + " ... ";
+
+        if (request.getQueryString() != null)
+        {
+            for(String q : request.getQueryString().split("&"))
+            {
+                if (q.contains("=")) 
+                {
+                    String variable = q.split("=")[0];
+                    String value = q.split("=")[1];
+                    requestText += "   " + variable + " = " + value;                                         
+                }
+                else               
+                {
+                    requestText += "   Invalid query string component (" + q + ")";
+                }
+            }
+        }
+        else
+        {
+            requestText += "   No query string supplied";
+        }
+        System.out.println(requestText);
+        history.append("<p>" + requestText + "</p>");
+
+        StringBuilder responseText = new StringBuilder();
+        responseText.append("<!DOCTYPE html>\n");
+        responseText.append("<html>\n");
+        responseText.append("<body>\n");
+        responseText.append("<h1>Welcome to my tiny Java web server!</h1>\n");
+        responseText.append("<hr /><h3>Messages recived so far:</h3>\n");
+        responseText.append(history);           
+        responseText.append("</body>\n");
+        responseText.append("</html>\n");        
+        response.getWriter().println(responseText);
+
+        baseRequest.setHandled(true);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(80);
+        server.setHandler(new HTTPRequestHandler());
+        server.start();
+        System.out.println("* * * Server is live on IP " + Inet4Address.getLocalHost().getHostAddress() + " * * *");
+        System.out.println("Close with CTRL+SHIFT+R in BlueJ main window or CTRL+C on command line.");
+        System.out.println("-----------------------------------------------------------------------");      
+        server.join();
+    }
+}
